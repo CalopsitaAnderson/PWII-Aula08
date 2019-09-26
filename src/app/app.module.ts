f9e6a1fd163f9e5e9a3e56e0a9ef8b6dbdced47c
niluiz <nicoleduarteluna@hotmail.com>
2019-09-25 22:44:25 -0300

    Aula 8

---
+++ src/app/app.module.ts
@@ -0,0 +1,30 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { InterpolationBindingComponent } from './interpolation-binding/interpolation-binding.component';
+import { PropertyBindingComponent } from './property-binding/property-binding.component';
+import { TwoWayBindingComponent } from './two-way-binding/two-way-binding.component';
+import { FormsModule } from '@angular/forms';
+import { NgIfComponent } from './ng-if/ng-if.component';
+import { NgSwitchCaseComponent } from './ng-switch-case/ng-switch-case.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    InterpolationBindingComponent,
+    PropertyBindingComponent,
+    TwoWayBindingComponent,
+    NgIfComponent,
+    NgSwitchCaseComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
